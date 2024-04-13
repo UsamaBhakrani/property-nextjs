6acd62341a3d2d0a874aad10d569ca53c0a2be1b
Mohammed Usama <usamabhakrani@gmail.com>
2024-04-13 14:40:47 +0500

    initial pages

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <h1 className="text-3xl">Welcome</h1>
+      <Link href={`/properties`}>Properties</Link>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
---
+++ app/properties/page.jsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+import React from "react";
+
+const PropertiesPage = () => {
+  return <Link href="/">back to HomePage</Link>;
+};
+
+export default PropertiesPage;
