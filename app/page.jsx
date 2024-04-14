73e51a5054fc7c23f6ebd27d7a9746f5f4615909
UsamaBhakrani <usamabhakrani@gmail.com>
2024-04-14 21:55:47 +0500

    HomeProperties Random

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,15 @@
-import Link from "next/link";
 import React from "react";
 import "@/assets/styles/globals.css";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 };
