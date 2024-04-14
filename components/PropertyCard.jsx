27ae76ece58f555c34c24d4454d6d3653faac95b
Usama Bhakrani <usamabhakrani@gmail.com>
2024-04-14 18:01:52 +0500

    dynamic paths

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import img from "@/assets/images/logo.png";
 import { getRates } from "@/utils/helpers";
 
 const PropertyCard = ({ property }) => {
@@ -72,7 +71,7 @@ const PropertyCard = ({ property }) => {
             <span className="text-orange-700"> {location.city} </span>
           </div>
           <Link
-            href={`/property/${_id}`}
+            href={`/properties/${_id}`}
             className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Details
