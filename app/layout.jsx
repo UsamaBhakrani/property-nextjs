00619f594cc17bc7923dae3ac797785bbd9dc4d1
Mohammed Usama <usamabhakrani@gmail.com>
2024-04-15 15:46:16 +0500

    Setup AuthProvider

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,16 +2,19 @@ import Footer from "@/components/Footer";
 import "../assets/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import React from "react";
+import AuthProvider from "@/components/AuthProvider";
 
 const HomeLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
