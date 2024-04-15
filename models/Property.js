a39cdbb21dbe17bbf44323c36e8bb2044fdb2977
Mohammed Usama <usamabhakrani@gmail.com>
2024-04-15 11:23:30 +0500

    Property and User Models

---
+++ models/Property.js
@@ -0,0 +1,74 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+const PropertySchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    location: {
+      street: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      state: {
+        type: String,
+      },
+      zipcode: {
+        type: String,
+      },
+    },
+    beds: { type: String, required: true },
+    baths: { type: String, required: true },
+    square_feet: { type: String, required: true },
+    amenities: [{ type: String }],
+    rates: {
+      nightly: {
+        type: Number,
+      },
+      weekly: {
+        type: Number,
+      },
+      monthly: {
+        type: Number,
+      },
+    },
+    seller_info: {
+      name: {
+        type: String,
+      },
+      email: {
+        type: String,
+      },
+      phone: {
+        type: String,
+      },
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+    is_featured: { Type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const Property = models.Property || model("Property", PropertySchema);
+
+export default Property;
